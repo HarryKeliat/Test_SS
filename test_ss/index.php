44f2a8a74df1872b6e4d40e86f08c3f80a406292
HarryKeliat <53415547+HarryKeliat@users.noreply.github.com>
2019-08-08 11:21:08 +0700

    Add files via upload

---
+++ test_ss/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test SS</title>
+	<link rel="stylesheet" href="Style/Utama/HAF.css">
+	<link rel="stylesheet" href="Style/index.css">
+	<link rel='stylesheet' href="Style/Utama/opensans.css">
+	<link rel="stylesheet" href="Style/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="Style/Bootstrap/bootstrap.min.css">
+	<script src="JS/jquery-3.3.1.slim.min.js"></script>
+	<script src="JS/popper.min.js" ></script>
+	<script src="JS/bootstrap.min.js" ></script>
+	<script src='JS/jquery-3.3.1.min.js'></script>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+</head>
+<body>
+		<!-- Header & Navigasi -->
+		<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #696969;">
+			<a href="#" onclick="myFunction()" style="text-decoration: none;"><h4 style="color: white">Logo Website</h4></a>
+		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="border:2px solid white;">
+		    <i class="fa fa-navicon" style="color: white;"></i>
+		  	</button>
+		  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    	<ul class="navbar-nav ml-auto" style="margin-right: 10px;">
+		      		<li class="nav-item">
+					<a class="nav-link" href="#" style="color: #ff0040; margin-right: 20px;">Beranda</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#" style="color: white; margin-right: 20px;">Koleksi Film</a>
+					</li>
+					<li class="nav-item">
+					<a class="nav-link" href="#" style="color: white; margin-right: 20px;">Tentang Kami</a>
+					</li>
+					<li class="nav-item">
+					<a class="nav-link" href="#" style="color: white; margin-right: 20px;">Hubungi Kami</a>
+		    	</ul>
+		  	</div>
+		</nav>
+		<img src="Image/avengers.jpg" style="width: 100%; height: auto; margin-bottom: 20px;">
+  		
+		<!-- Content -->
+		<div class="container-fluid" style="margin-bottom: 30px;">
+			<div class="row">
+		    	<div class="col-lg-2">
+		    		<img src="Image/alladin.jpg" class="img-thumbnail">
+		    		<h5>ALLADIN</h5>
+		    	</div>
+		    	<div class="col-lg-2">
+		    		<img src="Image/alladin.jpg" class="img-thumbnail">
+		    		<h5>ALLADIN</h5>
+		    	</div>
+		    	<div class="col-lg-2">
+		    		<img src="Image/alladin.jpg" class="img-thumbnail">
+		    		<h5>ALLADIN</h5>
+		    	</div>
+		    	<div class="col-lg-2">
+		    		<img src="Image/alladin.jpg" class="img-thumbnail">
+		    		<h5>ALLADIN</h5>
+		    	</div>
+		    	<div class="col-lg-2">
+		    		<img src="Image/alladin.jpg" class="img-thumbnail">
+		    		<h5>ALLADIN</h5>
+		    	</div>
+		    	<div class="col-lg-2">
+		    		<img src="Image/alladin.jpg" class="img-thumbnail">
+		    		<h5>ALLADIN</h5>
+		    	</div>
+		    	<div class="col-lg-4">
+		    		<div class="embed-responsive embed-responsive-16by9">
+					  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/foyufD52aog" allowfullscreen></iframe>
+					</div>
+					<h5>ALLADIN Trailer</h5>
+		    	</div>
+		    	<div class="col-lg-4">
+		    		<div class="embed-responsive embed-responsive-16by9">
+					  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/foyufD52aog" allowfullscreen></iframe>
+					</div>
+					<h5>ALLADIN Trailer</h5>
+		    	</div>
+		    	<div class="col-lg-4">
+		    		<div class="embed-responsive embed-responsive-16by9">
+					  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/foyufD52aog" allowfullscreen></iframe>
+					</div>
+					<h5>ALLADIN Trailer</h5>
+		    	</div>
+			</div>
+		</div>
+		
+		<!-- Footer -->
+		<footer class="footer-distributed">
+			<div class="col-lg-3">
+				<h3 style="color: white; font-weight: bold; text-align: center;">Logo Website</h3>
+					<p>
+						awiodjajdioaj
+					</p>
+			</div>
+			<div class="col-lg-3">
+				<h2><strong>Tentang Kami</strong></h2>
+					<p>
+						awiodjajdioaj
+					</p>
+			</div>
+			<div class="col-lg-3">
+				<h2><strong>Kontak Kami</strong></h2>
+					<p>
+						awiodjajdioaj
+					</p>
+			</div>
+			<div class="col-lg-3">
+				<h2><strong>Follow Us</strong></h2>
+					<p>
+						<a><i class="fa fa-twitter" style="font-size: 50px; margin-right: 20px;"></i></a>
+						<a><i class="fa fa-instagram" style="font-size: 50px; margin-right: 20px;"></i></a>
+						<a><i class="fa fa-facebook" style="font-size: 50px;"></i></a>
+					</p>
+			</div>
+			</div>
+		</footer>
+	<script type="text/javascript">
+		function myFunction() {
+  		alert("Selamat Datang Bro!!!");
+		}
+	</script>
+</body>
+</html>
